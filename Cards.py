bd5859d3345b46bfe4d0bdd94cc555c487dfe542
Darya K. <danechkakaz@gmail.com>
2023-11-20 21:13:44 +0300

    adjustment

--- Cards.py
+++ Cards.py
@@ -39,3 +39,7 @@ class Card:
         v = self.values[self.value] + " of " \
         + self. suits[self.suit]
         return v
+    
+card1 = Card(10, 2)
+card2 = Card(11, 3)
+print(card1 < card2)
